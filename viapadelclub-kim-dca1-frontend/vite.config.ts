61b4660b92e30626eaba0d01ab10cadbb143f8ca
EQKim <114055097+EQKim@users.noreply.github.com>
2026-05-19 21:57:50 +0200

    Add GitHub Pages deploy and set Vite base

--- viapadelclub-kim-dca1-frontend/vite.config.ts
+++ viapadelclub-kim-dca1-frontend/vite.config.ts
@@ -3,6 +3,7 @@ import plugin from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/ViaPadelClub-Kim-DCA1-Frontend/',
     plugins: [plugin()],
     server: {
         port: 61510,
